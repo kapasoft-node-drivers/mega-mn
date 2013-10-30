512621c206459db68f3e92d673fd4277ce6d75a6
Margots Kapacs <makapacs@hotmail.com>
2013-10-30 10:43:03 -0500

    Release=-0.0.1: deploying mega-mn

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.controller_url = process.env.CONTROLLER_URL || '/rental/api/';
+config.controller_url = process.env.CONTROLLER_URL || '/mega-mn/api/';
 config.controller_host = process.env.CONTROLLER_HOST || 'localhost';
 config.controller_port = process.env.CONTROLLER_PORT || 8080;
 
--- env.sh
+++ env.sh
@@ -4,6 +4,6 @@
 #echo export CONTROLLER_HOST=min-controller.cloudfoundry.com
 #echo export CONTROLLER_PORT=80
 
-echo export CONTROLLER_URL=/rental/api/
+echo export CONTROLLER_URL=/mega-mn/api/
 echo export CONTROLLER_HOST=localhost
 echo export CONTROLLER_PORT=8080
